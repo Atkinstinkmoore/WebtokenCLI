1a2118df86c6ca45470fce019b0f32087fbfe155
Atkinkstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2022-03-17 16:56:18 +0100

    Add project files.

---
+++ WebtokenCLI/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Cocona;
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = CoconaApp.CreateBuilder();
+
+var app = builder.Build();
+
+app.AddCommand("validate", (string token) =>
+{
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var jwtValid = tokenHandler.CanReadToken(token);
+
+    Console.Write("Token is ");
+
+    if (jwtValid)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Valid");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid");
+    }
+}).WithDescription("Checks if a string is a valid JWToken");
+
+app.Run();
