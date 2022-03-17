6ed35ad7ec17ad7b99702c8620f63e97ccb23e3f
Atkinstrinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2022-03-17 19:29:38 +0100

    optional printout of parsed token

--- WebtokenCLI/Program.cs
+++ WebtokenCLI/Program.cs
@@ -6,15 +6,16 @@ var builder = CoconaApp.CreateBuilder();
 
 var app = builder.Build();
 
-app.AddCommand("validate", ([Argument(Description = "The string to validate")]string token) =>
+app.AddCommand("validate", ([Option(name: "output", shortNames: new char[] {'o'}, Description = "Prints the parsed token to the console if valid" )] bool print,
+    [Argument(Description = "The string to validate")]string token) =>
 {
-    ValidateToken(token);
+    ValidateToken(token, print);
 
 }).WithDescription("Checks if a string is a valid JWToken");
 
 await app.RunAsync();
 
-void ValidateToken(string token)
+void ValidateToken(string token, bool? print)
 {
     var tokenHandler = new JwtSecurityTokenHandler();
     var jwtValid = tokenHandler.CanReadToken(token);
@@ -25,6 +26,12 @@ void ValidateToken(string token)
     {
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Valid");
+        if(print != null && print == true)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            var parsedToken = tokenHandler.ReadJwtToken(token);
+            Console.WriteLine(parsedToken);
+        }
     }
     else
     {
